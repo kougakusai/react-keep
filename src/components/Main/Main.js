3c7be14b36d04b439d8831f9cf7f076c83ba17f4
TomJPN <ryu.12.22.sei.2000.tom@gmail.com>
2021-09-23 21:21:17 +0900

    Cardコンポーネントの作成

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CardInput from '../Card/CardInput';
+import Card from '../Card/Card';
 
 const Main = () => {
   return (
     <div className='flex flex-col space-y-3'>
       <CardInput />
       <div>
-        <p>Card</p>
+        <Card content='Cardコンポーネント1' />
+        <Card content='Cardコンポーネント2' />
       </div>
     </div>
   );
