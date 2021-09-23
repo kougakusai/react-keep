b08861e41094479c92983e04d2b976f21d17d140
TomJPN <ryu.12.22.sei.2000.tom@gmail.com>
2021-09-24 00:00:44 +0900

    #7 Cardlistの作成

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = (props) => {
   return (
-    <div className='py-2 px-4 mt-2 rounded-md border border-gray-400'>
+    <div className='py-2 px-4 rounded-md border border-gray-400'>
       <p>{props.content}</p>
     </div>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import CardInput from '../Card/CardInput';
-import Card from '../Card/Card';
+import Cardlist from '../Card/Cardlist';
 
 const Main = () => {
   return (
     <div className='flex flex-col space-y-3'>
       <CardInput />
       <div>
-        <Card content='Cardコンポーネント1' />
-        <Card content='Cardコンポーネント2' />
+        <Cardlist />
       </div>
     </div>
   );
